85c1a001070fcf9f8512e4cea7267cfc941abc40
hs917ouo <hs917ouo@naver.com>
2024-09-05 14:02:07 +0900

    update

--- hana4/html/tw/src/example.js
+++ hana4/html/tw/src/example.js
@@ -1,1 +1,26 @@
-console.log("hi");
+hong = { id: 1, name: "Hong" };
+lee = { id: 2, name: "Lee" };
+unknown = { id: 3 };
+ex = { name: "Hwang" };
+
+function f1(user) {
+  console.log(user.id, user.name);
+}
+
+function f2({ id, name } = user) {
+  console.log(user);
+} // 오답, = user는 쓰는거 아님
+
+const f3 = ({ ...user }) => {
+  console.log(user);
+};
+
+// f1(hong);
+// f2(unknown);
+//f3(hong);
+
+function f4({ id, name }) {
+  console.log(id, name);
+} // f2의 정답
+
+f4(lee);
